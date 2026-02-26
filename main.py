ddbf234d870287b8552c644d55286e55a4f440b6
colinknox <colinwknox@gmail.com>
2026-02-25 19:53:36 -0500

    created HighScoreGame class

--- main.py
+++ main.py
@@ -9,19 +9,36 @@ class Game:
     def determine_winner(self, player1_score, player2_score):
         raise NotImplementedError("Must implement winner logic")
 
+class HighScoreGame(Game):
+    def __init__(self, player1_name, player2_name):
+        super().__init__(player1_name, player2_name)
+
+    def calculate_score(self, player1_data, player2_data):
+        return (player1_data, player2_data)
+
+    def determine_winner(self, player1_score, player2_score):
+        if player1_score > player2_score:
+            return self.player1_name
+        elif player2_score > player1_score:
+            return self.player2_name 
+        else:
+            return "Tie"
 
 
-game = Game("Chris", "Corey")
+
+# game = Game("Chris", "Corey")
+high_score_game = HighScoreGame("Big Boss", "Eva")
 
 print(f"""
-PLAYER 1 NAME = {game.player1_name}
-PLAYER 2 NAME = {game.player2_name}
-DETERMINE WINNER = {game.determine_winner(game.player1_name, game.player2_name)}
+PLAYER 1 NAME = {high_score_game.player1_name}
+PLAYER 2 NAME = {high_score_game.player2_name}
+CALCULATE SCORE = {high_score_game.calculate_score(93, 45)}
+DETERMINE WINNER = {high_score_game.determine_winner(52, 51)}
 """)
 
 # print(f"""
-# PLAYER 1 NAME = {game.player1_name}
-# PLAYER 2 NAME = {game.player2_name}
-# CALCULATE SCORE = {game.calculate_score(game.player1_name, game.player2_name)}
-# DETERMINE WINNER = {game.determine_winner(game.player1_name, game.player2_name)}
+# PLAYER 1 NAME = {high_score_game.player1_name}
+# PLAYER 2 NAME = {high_score_game.player2_name}
+# CALCULATE SCORE = {high_score_game.calculate_score()}
+# DETERMINE WINNER = {high_score_game.determine_winner(50, 51)}
 # """)
